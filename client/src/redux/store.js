67f3ed9d188c2118972d998697296eadc1955fbc
hanadiOkla <eng.haanadiokla@gmail.com>
2026-04-27 21:28:00 +0300

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from '../redux/user/userSlice.js';
+import { persistReducer, persistStore } from 'redux-persist'; // تأكدي من الاسم هنا: persistReducer
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+// هنا التعديل: استدعاء الدالة persistReducer وتخزينها في متغير persistedReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer:  { user: userReducer},
-  middleware: (getDefaultMiddleware) => 
+  reducer: persistedReducer, // تأكدي أن الاسم يطابق المتغير في السطر السابق
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-        serializableCheck:false,
+      serializableCheck: false,
+    }),
+});
 
-    })
-})
-
+export const persistor = persistStore(store);
