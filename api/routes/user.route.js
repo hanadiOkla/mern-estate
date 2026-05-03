fc444ba92e9b2286eb9031fcda17c95caabdd410
hanadiOkla <eng.haanadiokla@gmail.com>
2026-05-03 20:42:36 +0300

    add delete user functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { test, updateUser } from '../controllers/user.controller.js'; // تأكدي من الاسم هنا
+import { deleteUser, test, updateUser } from '../controllers/user.controller.js'; // تأكدي من الاسم هنا
 import { verifyToken } from '../utils/verifyUser.js'; // تأكدي من استيراد verifyToken
 
 const router = express.Router();
 
 router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser); // التعديل هنا: verifyToken كـ middleware
+router.delete('/delete/:id', verifyToken, deleteUser); // التعديل هنا: verifyToken كـ middleware
 
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { usere } from 'react';
 import { getStorage, uploadBytesResumable, ref, getDownloadURL } from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserStart ,updateUserSuccess ,updateUserFailure } from '../redux/user/userSlice'; 
+import { updateUserStart ,updateUserSuccess ,updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice'; 
 import { useDispatch } from 'react-redux';
 
 function Profile() {
@@ -73,6 +73,24 @@ const handleSubmit =async (e) => {
   }
 }
 
+  const hanleDeleteUser =  async () => {
+    try{
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}` , {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if(data.success === false ){
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+
+
+    }catch(error){
+      dispatch(deleteUserFailure(error.message));
+    }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -124,7 +142,7 @@ const handleSubmit =async (e) => {
         </button>
       </form>
       <div className='flex justify-between mt-5 '>
-        <span className='text-red-700  cursor-pointer'>Delete account</span>
+        <span onClick={ hanleDeleteUser } className='text-red-700  cursor-pointer'>Delete account</span>
         <span className='text-red-700  cursor-pointer'>Sign out</span>
       </div>
       <p className='text-red-700 mt-5'>{error ? error: '' }</p>
