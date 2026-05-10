1c31fc44d2e05f1769bbecf79a371745dc81e616
hanadiOkla <eng.haanadiokla@gmail.com>
2026-05-10 23:35:14 +0300

    create update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import exporess from "express";
-import { creatListing, deleteListing } from "../controllers/listing.controller.js";
+import { creatListing, deleteListing, updateListing } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = exporess.Router();
 
 router.post('/create', verifyToken ,creatListing );
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id' , verifyToken , updateListing);
 
 export default router;
