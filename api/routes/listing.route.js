1bf981db53be936acebdf602cbb0e0f55703dda4
hanadiOkla <eng.haanadiokla@gmail.com>
2026-05-10 19:58:42 +0300

    complete delete user listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,10 @@
 import exporess from "express";
-import { creatListing } from "../controllers/listing.controller.js";
+import { creatListing, deleteListing } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = exporess.Router();
 
 router.post('/create', verifyToken ,creatListing );
+router.delete('/delete/:id', verifyToken, deleteListing);
 
 export default router;
