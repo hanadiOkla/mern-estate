6924b51cde8218a508d9e0556b0b6dc44e950c20
hanadiOkla <eng.haanadiokla@gmail.com>
2026-05-11 23:12:10 +0300

    create update oage

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -219,7 +219,9 @@ const handleSubmit =async (e) => {
             </Link>
             <div className=" flex flex-col items-center">
               <button onClick={ () => handleListingDelete(listing._id) } className='text-red-700'>Delete</button>
-              <button className='text-green-700'>Edit</button>
+              <Link to={`/update-listing/${listing._id}`}>
+                <button className='text-green-700'>Edit</button>
+              </Link>
             </div>
           </div>            
         )}
