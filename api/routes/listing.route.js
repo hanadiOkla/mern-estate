5c265ac76a321faf7c90b81f9362a2a3424f945b
hanadiOkla <eng.haanadiokla@gmail.com>
2026-05-16 23:57:59 +0300

    create search api

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import exporess from "express";
-import { creatListing, deleteListing, updateListing, getListing } from "../controllers/listing.controller.js";
+import { creatListing, deleteListing, updateListing, getListing, getListings } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = exporess.Router();
@@ -8,4 +8,5 @@ router.post('/create', verifyToken ,creatListing );
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id' , verifyToken , updateListing);
 router.get('/get/:id', getListing);
+router.get('/get', getListings);
 export default router;
