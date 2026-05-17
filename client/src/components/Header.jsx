2358459a1729836705bb6c3fb1ed7027ca409380
hanadiOkla <eng.haanadiokla@gmail.com>
2026-05-18 00:09:03 +0300

    complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import {FaSearch } from'react-icons/fa';
-import {Link, useNavigate} from 'react-router-dom';
+import {Link, useNavigate, useLocation} from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 
@@ -8,6 +8,7 @@ function Header() {
   const { currentUser } = useSelector((state) => state.user);
   const [ searchTerm, setSearchTerm ] = useState(''); 
   const navigate =useNavigate();
+  const location = useLocation();
   const handelSubmit = (e) =>{
     e.preventDefault();
     const urlParams = new URLSearchParams(window.location.search); 
@@ -17,12 +18,12 @@ function Header() {
   };
 
   useEffect( () => {
-    const urlParams = new URLSearchParams(window.location.search);
+    const urlParams = new URLSearchParams(location.search);
     const searchTermFormUrl = urlParams.get('searchTerm');
     if(searchTermFormUrl){
-      setSearchTerm
+      setSearchTerm(searchTermFormUrl)
     }
-  })
+  }, [location.search]);
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -39,6 +40,7 @@ function Header() {
             id=""  
             placeholder='Search...' 
             className='bg-transparent focus:outline-none w-24 sm:w-64'
+            value={searchTerm}
             onChange={ (e) => setSearchTerm(e.target.value)}
           />
           <button>
