e575ba24557ec684eff98427b9e538bdc81d7636
hanadiOkla <eng.haanadiokla@gmail.com>
2026-05-20 17:39:11 +0300

    Create Search Page

---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Search() {
+  return (
+    <div className='flex flex-col md:flex-row '>
+        <div className='p-7 border-b-2 sm:border-r-2 md:min-h-screen'>
+            <form action="">
+                <div className='flex items-center gap-2'>
+                    <label className='whitespace-nowrap'>Search Term:</label>
+                    <input 
+                        type="text"
+                        id='searchTerm'
+                        placeholder='Search...'
+                        className='border rounded-lg p-3 w-full'
+                    />
+                </div>
+                <div>
+                    <label htmlFor="">Type:</label>
+                </div>
+
+
+            </form>
+        </div>
+        <div className=''>
+            <h1>Listing results:</h1>
+        </div>
+    </div>
+  )
+}
