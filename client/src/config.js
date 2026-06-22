b1d3b8baad28214eb72924487edfce6f55c74fef
hanadiOkla <eng.haanadiokla@gmail.com>
2026-06-22 22:41:24 +0300

    fix config.js

--- client/src/config.js
+++ client/src/config.js
@@ -3,7 +3,7 @@
 const isProduction = import.meta.env.PROD;
 
 export const API_BASE_URL = isProduction
-  ? "https://mern-estate-api-14oy.onrender.com" // رابط الباك إند الحقيقي أونلاين
+  ? "https://mern-estate-api-l4oy.onrender.com" // رابط الباك إند الحقيقي أونلاين
   : "http://localhost:3000";                    // رابط الباك إند المحلي للمطوّر
 
 export const FIREBASE_API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
