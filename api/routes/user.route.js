18f26b1b1b8c53ecd6234d75a99f4659f9da8542
hanadiOkla <eng.haanadiokla@gmail.com>
2026-07-02 00:21:02 +0300

    chore: setup docker-compose environment and fix user controller exports

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { deleteUser, test, updateUser, getUserListings, getUser } from '../controllers/user.controller.js'; // تأكدي من الاسم هنا
+import { deleteUser, updateUser, getUserListings, getUser } from '../controllers/user.controller.js'; // تأكدي من الاسم هنا
 import { verifyToken } from '../utils/verifyUser.js'; // تأكدي من استيراد verifyToken
 
 const router = express.Router();
 
-router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser); // التعديل هنا: verifyToken كـ middleware
 router.delete('/delete/:id', verifyToken, deleteUser); // التعديل هنا: verifyToken كـ middleware
 router.get('/listings/:id' , verifyToken, getUserListings);
